e1595e76ab91d02f6375ab883a0bda01462c8d1b
Akhilreddymalladi <105489408+Akhil-132@users.noreply.github.com>
2023-10-17 14:53:09 +0530

    Update settings.py

--- Brigade/Brigade/settings.py
+++ Brigade/Brigade/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER': 'django_user',
-        'PASSWORD': 'Password',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
